477eb7a46d65b59db5ba96ba20c1a4f2c263af56
Aneta Koppernock <akozuchowska@hotmail.com>
2018-02-15 23:59:20 +0100

    zadanie 8.6

---
+++ README.md
@@ -0,0 +1,2 @@
+# zadanie_8-5
+https://kasstom.github.io/zadanie_8-5/
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+//zmienne
+var a = prompt('Podaj pierwszą liczbę'),
+	b = prompt('Podaj drugą liczbę'),
+	wynikDzialania = (a * a) - (2 * a * b) - (b * b);
+
+//wyskakujacy alert z wynikiem
+document.write('Działanie: (a * a) - (2 * a * b) - (b * b)= ' + wynikDzialania);	
+//zapis w consoli
+console.log('Wynik działania: ' + wynikDzialania);
+
+
+// instrukcje warunkowe
+if ( wynikDzialania >= '1') {
+	console.log('Wynik działania jest dodatni!');
+		} 
+else if ( wynikDzialania <= '-1') {
+	console.log('Wynik działania jest ujemny!');
+		}
+else {
+	console.log('Wynik działania jest równy 0');
+	}
